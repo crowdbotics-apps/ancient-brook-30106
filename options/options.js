8cde3c39b11df02cdd026cdc70060aa5ae47f232
Crowdbotics <team@crowdbotics.com>
2021-08-25 18:42:26 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "ancient_brook_30106Identifier",
+  url: "https://ancient_brook_30106Identifier.botics.co",
+  api: "https://ancient_brook_30106Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
